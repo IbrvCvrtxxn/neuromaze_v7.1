651091337fbfa445a897999953f59ade3c31da53
IbrvCvrtxxn <iibrahim.ismayilovv@gmail.com>
2023-07-05 02:53:57 +0200

    newMonologue

--- Neuromaze v7.1/Assets/Script/IngameStoryTrigger.cs
+++ Neuromaze v7.1/Assets/Script/IngameStoryTrigger.cs
@@ -1,52 +1,3 @@
-// using UnityEngine;
-// using UnityEngine.UI;
-
-// public class IngameStoryTrigger : MonoBehaviour
-// {
-//     public Canvas canvas;
-//     public float activationDuration = 5f;
-
-//     private bool isTriggered = false;
-//     private bool isCanvasActive = false;
-//     private float timer = 0f;
-
-//     private void Update()
-//     {
-//         if (isTriggered && isCanvasActive)
-//         {
-//             timer += Time.deltaTime;
-
-//             if (timer >= activationDuration)
-//             {
-//                 DeactivateCanvas();
-//             }
-//         }
-//     }
-
-//     private void OnTriggerEnter(Collider other)
-//     {
-//         if (other.CompareTag("Player") && !isTriggered && !isCanvasActive)
-//         {
-//             ActivateCanvas();
-//         }
-//     }
-
-//     private void ActivateCanvas()
-//     {
-//         isTriggered = true;
-//         isCanvasActive = true;
-//         canvas.gameObject.SetActive(true);
-//         timer = 0f;
-//     }
-
-//     private void DeactivateCanvas()
-//     {
-//         isCanvasActive = false;
-//         canvas.gameObject.SetActive(false);
-//     }
-// }
-
-
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -58,7 +9,6 @@ public class IngameStoryTrigger : MonoBehaviour
     private bool isTriggered = false;
     private bool isCanvasActive = false;
     private float timer = 0f;
-    private static bool isAnyCanvasActive = false;
 
     private void Update()
     {
@@ -75,7 +25,7 @@ public class IngameStoryTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && !isTriggered && !isCanvasActive && !isAnyCanvasActive)
+        if (other.CompareTag("Player") && !isTriggered && !isCanvasActive)
         {
             ActivateCanvas();
         }
@@ -85,7 +35,6 @@ public class IngameStoryTrigger : MonoBehaviour
     {
         isTriggered = true;
         isCanvasActive = true;
-        isAnyCanvasActive = true;
         canvas.gameObject.SetActive(true);
         timer = 0f;
     }
@@ -93,7 +42,58 @@ public class IngameStoryTrigger : MonoBehaviour
     private void DeactivateCanvas()
     {
         isCanvasActive = false;
-        isAnyCanvasActive = false;
         canvas.gameObject.SetActive(false);
     }
 }
+
+
+// using UnityEngine;
+// using UnityEngine.UI;
+
+// public class IngameStoryTrigger : MonoBehaviour
+// {
+//     public Canvas canvas;
+//     public float activationDuration = 5f;
+
+//     private bool isTriggered = false;
+//     private bool isCanvasActive = false;
+//     private float timer = 0f;
+//     private static bool isAnyCanvasActive = false;
+
+//     private void Update()
+//     {
+//         if (isTriggered && isCanvasActive)
+//         {
+//             timer += Time.deltaTime;
+
+//             if (timer >= activationDuration)
+//             {
+//                 DeactivateCanvas();
+//             }
+//         }
+//     }
+
+//     private void OnTriggerEnter(Collider other)
+//     {
+//         if (other.CompareTag("Player") && !isTriggered && !isCanvasActive && !isAnyCanvasActive)
+//         {
+//             ActivateCanvas();
+//         }
+//     }
+
+//     private void ActivateCanvas()
+//     {
+//         isTriggered = true;
+//         isCanvasActive = true;
+//         isAnyCanvasActive = true;
+//         canvas.gameObject.SetActive(true);
+//         timer = 0f;
+//     }
+
+//     private void DeactivateCanvas()
+//     {
+//         isCanvasActive = false;
+//         isAnyCanvasActive = false;
+//         canvas.gameObject.SetActive(false);
+//     }
+// }
